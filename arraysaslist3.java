0c6406086effba34abe20b3a51db6c0cc0da902c
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-01-21 19:11:13 +0530

    Add files via upload

---
+++ arraysaslist3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arraysaslist3
+{
+public static void main(String g[])
+{
+String str[]={"a","b","c","d"};
+List <String>al=new ArrayList<>();
+Collections.addAll(al,str);
+System.out.println(al);
+}
+}
